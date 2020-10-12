fdaf5161fe01c60f12c3f58eb801e63800ffc755
Julio Izquierdo Mejia <julioizquierdomejia@Julios-MacBook-Pro.local>
2020-10-12 16:45:41 -0500

    Configracion de .env y database configuracion de multiple base de datos

--- config/database.php
+++ config/database.php
@@ -63,6 +63,19 @@ return [
             ]) : [],
         ],
 
+
+        'mysql_moodle'  => [
+            'driver'     => 'mysql',
+            'host'       => env('MOODLE_HOST', 'localhost'),
+            'database'   => env('MOODLE_DATABASE', 'forge'),
+            'username'   => env('MOODLE_USERNAME', 'forge'),
+            'password'   => env('MOODLE_PASSWORD', ''),
+            'charset'    => 'utf8',
+            'collation'  => 'utf8_unicode_ci',
+            'prefix'     => 'mdl_cv',
+            'strict'     => false,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
