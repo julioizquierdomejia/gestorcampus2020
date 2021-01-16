6e849c35d980b32b43497dcf8db3a555f7616875
Julio Izquierdo Mejia <julioizquierdomejia@Julios-MacBook-Pro.local>
2021-01-16 09:01:38 -0500

    revision de codigo al registrar

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -33,7 +33,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    //protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
@@ -242,9 +242,5 @@ class RegisterController extends Controller
         return $user;   
     }
 
-    protected function registered(Request $request, $user)
-    {
-        return redirect()->route('login');
 
-    }
 }
