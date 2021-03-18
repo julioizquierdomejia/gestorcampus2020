0d8943806b6174ef6c3bd0ed608c7d30dafbc5b4
Julio Izquierdo Mejia <julioizquierdomejia@Julios-MacBook-Pro.local>
2021-03-18 15:32:20 -0500

    rutas de css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,9 +13,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
 
-
-    <link rel="stylesheet" type="text/css" href="css/campus.css">
-    <link href="css/app.css" rel="stylesheet" />
+    <link href="{{ asset('css/campus.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
 
     <title>Campus Aspefam</title>
   </head>
