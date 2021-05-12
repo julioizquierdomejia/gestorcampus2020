7601c4fb58c75ab6ea9fa79736f715bb9d6097c8
Julio Izquierdo Mejia <julioizquierdomejia@Julios-MacBook-Pro.local>
2021-05-11 21:05:23 -0500

    cruce de notas query

--- resources/views/notas/index.blade.php
+++ resources/views/notas/index.blade.php
@@ -7,7 +7,8 @@
 <div class="row components_content p-5">
 	<div class="row">
 		<div class="col">
-			<h5 class="text-danger">Mi progreso - {{$percent}}%</h5>
+			<h5>Mi progreso</h5>
+			<p>[ {{$cant_notas}} evaluaciones de $cant_notas_existente / {{$percent}}% ]</p>
 			<div class="progress">
 			  <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: {{$percent}}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
 			</div>
