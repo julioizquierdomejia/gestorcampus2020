5aeb2510f5fda63288b20613ae215ae2534c1abc
Julio Izquierdo Mejia <julioizquierdomejia@Julios-MacBook-Pro.local>
2021-05-11 20:31:24 -0500

    cruce de notas query

--- resources/views/notas/index.blade.php
+++ resources/views/notas/index.blade.php
@@ -9,11 +9,13 @@
 		<div class="col-4">
 			<h2>Mis notas </h2>
 
+			{{$percent}}
+			
 			<div class="progress" style="height: 1px;">
-			<div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="{{$percent}}" aria-valuemin="0" aria-valuemax="100"></div>
+			<div class="progress-bar" role="progressbar" style="width: {{$percent}}%;" aria-valuenow="{{$percent}}" aria-valuemin="0" aria-valuemax="100"></div>
 			</div>
 			<div class="progress" style="height: 20px;">
-			<div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="{{$percent}}" aria-valuemin="0" aria-valuemax="100"></div>
+			<div class="progress-bar" role="progressbar" style="width: {{$percent}}%;" aria-valuenow="{{$percent}}" aria-valuemin="0" aria-valuemax="100"></div>
 			</div>
 
 
