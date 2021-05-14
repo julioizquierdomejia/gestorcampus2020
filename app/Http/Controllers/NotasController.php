237d1928219ccfdc2b76e1ddc522010c7ef87b57
Julio Izquierdo Mejia <julioizquierdomejia@Julios-MacBook-Pro.local>
2021-05-13 22:29:19 -0500

    notas

--- app/Http/Controllers/NotasController.php
+++ app/Http/Controllers/NotasController.php
@@ -102,7 +102,7 @@ class NotasController extends Controller
                         ->where('user_id', $id_usuario_moodle)
                         ->first();
 
-        dd($id_matricula->user_id);
+        dd($id_matricula->count());
 
 
         return view('notas.index', compact('usuario', 'quiz', 'cant_notas', 'notas', 'cant_notas_existente', 'statusCurso', 'percent', 'curso', 'promedio', 'user'));
