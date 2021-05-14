34360978c0f0b02cb40ff77d2470305fbbeea950
Julio Izquierdo Mejia <julioizquierdomejia@Julios-MacBook-Pro.local>
2021-05-14 12:36:59 -0500

    certificados

--- resources/views/certificates/index.blade.php
+++ resources/views/certificates/index.blade.php
@@ -7,7 +7,7 @@ hola soy el certificado
 <div class="container mt-5">
 	<div class="row">
 		<div class="col">
-			<img src="{{ asset('/certificados/base.png') }}">			
+			<img src="{{ asset('/certificados/base.png') }}" class="img-fluid" alt="...">
 		</div>
 	</div>
 
