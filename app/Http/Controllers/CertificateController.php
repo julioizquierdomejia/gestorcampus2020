fa8bded4bbdef73a888c60adf35e25174b35b13c
Julio Izquierdo Mejia <julioizquierdomejia@Julios-MacBook-Pro.local>
2021-05-17 18:17:05 -0500

    certificados

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -58,7 +58,7 @@ class CertificateController extends Controller
     public function show(Certificate $certificate)
     {
         //
-        dd($certificate);
+        dd($certificate->id);
         $img = Image::make('certificados/base.png');
 
         //detecto el ancho te la imagen, para determinar el centro de la misma
