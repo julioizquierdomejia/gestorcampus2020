3f42c1d9c45254397305d86b599a25b8c0c7ddef
Julio Izquierdo Mejia <julioizquierdomejia@Julios-MacBook-Pro.local>
2021-05-17 17:32:29 -0500

    certificados

--- resources/views/certificates/index.blade.php
+++ resources/views/certificates/index.blade.php
@@ -6,7 +6,7 @@
 <div class="container mt-5 mb-5">
 	<div class="row">
 		<div class="col">
-			<div class="certificado" style="position: absolute;">
+			<div class="certificado">
 				<img src="{{ asset('/certificados/base.png') }}" class="img-fluid" alt="...">	
 				<div class="nombre" style="position: absolute; top: 100px; left: 400px;">
 					<p>Julio Izquierdo Mejia</p>
